3e7b2a68419bb0a3a96cb4baad911dff060d6a02
pea-sys <pea98258@gmail.com>
2024-09-01 19:41:44 +0900

    Astroアイランドへの出航

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://master--pea-sys-blog.netlify.app"
+  site: "https://master--pea-sys-blog.netlify.app",
+  integrations: [preact()]
 });
