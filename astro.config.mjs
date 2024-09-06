36133b727b0d03654255206217d803e62b9c56a9
pea-sys <pea98258@gmail.com>
2024-09-06 20:42:13 +0900

    siemapを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from 'astro/config';
 
 import preact from "@astrojs/preact";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://master--pea-sys-blog.netlify.app",
-  integrations: [preact()]
+  integrations: [preact(), sitemap()]
 });
