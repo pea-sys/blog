55dd2477a892638cbad6aa6bde7ca1506ac5f406
pea-sys <pea98258@gmail.com>
2024-09-12 06:45:41 +0900

    ブログタイトル修正

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,8 +2,8 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
   return rss({
-    title: 'Astro学習者 | ブログ',
-    description: 'Astroを学ぶ旅',
+    title: 'Continuing daily | ブログ',
+    description: '日々の積み重ねを記録するブログ',
     site: context.site,
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>ja-jp</language>`,
